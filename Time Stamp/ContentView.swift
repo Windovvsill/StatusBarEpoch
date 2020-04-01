14802339822ff4e2fc647efe7a5da899851de56f
Steve <svezer@live.ca>
2020-04-01 11:27:38 -0700

    Initial Commit

---
+++ Time Stamp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Time Stamp
+//
+//  Created by Steve on 2020-04-01.
+//  Copyright © 2020 Steve. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
