1ba67d20fb92b6a609ee51eda0c01d6fe11fd41d
Steve <svezer@live.ca>
2020-09-10 19:15:35 -0700

    encapsulate the time scale specific logic and fix output not always rendering automatically

--- Time Stamp/TimeStampViewController.swift
+++ Time Stamp/TimeStampViewController.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class TimeStampViewController: NSViewController, NSTextFieldDelegate {
+    
+    // This number is 1975 when interpreted in ms and year 6976 in seconds
+    let timeScaleThreshold = 158000000000.0
 
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var outputLabel: NSTextField!
@@ -19,6 +22,9 @@ class TimeStampViewController: NSViewController, NSTextFieldDelegate {
         
         // Do view setup here.
         textField.delegate = self
+        
+        // Show something in the textfield and output on boot
+        writeNowInTextField()
     }
     
     @objc func textFieldDidChange(_ textField: NSTextField) {
@@ -26,58 +32,73 @@ class TimeStampViewController: NSViewController, NSTextFieldDelegate {
     }
     
     public func controlTextDidEndEditing(_ obj: Notification) {
-        renderOutput(obj: obj)
+        renderOutput()
     }
     
     public func controlTextDidChange(_ obj: Notification) {
-        renderOutput(obj: obj)
+        renderOutput()
     }
     
-    func renderOutput(obj: Notification) {
+    func renderOutput() {
         // Check the identifier to be sure you have the correct textfield if more are used
-        if let textField = obj.object as? NSTextField, self.textField.identifier == textField.identifier {
-            print("\n\nMy own textField = \(String(describing: self.textField))\nNotification textfield = \(textField)")
-            print("\nChanged text = \(textField.stringValue)\n")
+//        if let textField = obj.object as? NSTextField, self.textField.identifier == textField.identifier {
             
-            let ts: Double
-            if (textField.doubleValue > 1580000000000) {
-                ts = textField.doubleValue / 1000
-                errorLabel.stringValue = "Note: assuming format is milliseconds"
-            } else {
-                ts = textField.doubleValue
-                errorLabel.stringValue = ""
-            }
+            let (scaledTs, timeUnitDescriptionString) = tsScaleFormat(ts: textField.doubleValue)
             
-            
-            let date = Date(timeIntervalSince1970: ts)
+            let date = Date(timeIntervalSince1970: scaledTs)
             let dateFormatter = DateFormatter()
-            dateFormatter.timeZone = TimeZone.current // Use the machine timezone
+            
+            // Use the machine timezeon and set the date formatting
+            dateFormatter.timeZone = TimeZone.current
             dateFormatter.locale = NSLocale.current
-            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss O" //Specify your format that you want
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss O"
             let strDate = dateFormatter.string(from: date)
             
+            errorLabel.stringValue = timeUnitDescriptionString
             outputLabel.stringValue = strDate
+//        }
+    }
+    
+    /**
+     * Determine whether to interpret the input as milliseconds or seconds
+     * and returns the scaled timestamp and the unit descriptor.
+     */
+    func tsScaleFormat(ts: Double) -> (Double, String) {
+        if (ts > timeScaleThreshold) {
+            return (ts / 1000, "milliseconds")
+        } else {
+            return (ts, "seconds")
         }
+    }
+    
+    func writeTsInTextField(ts: Double) {
+        textField.stringValue = String(format: "%.0f", (ts).rounded())
+        renderOutput()
+    }
+    
+    func writeNowInTextField() {
+        writeTsInTextField(ts: NSDate().timeIntervalSince1970)
     }
 }
 
 extension TimeStampViewController {
     @IBAction func now(_ sender: NSButton) {
-        textField.stringValue = String(format: "%.0f", (NSDate().timeIntervalSince1970).rounded())
+        writeNowInTextField()
     }
 }
 
 extension TimeStampViewController {
   // MARK: Storyboard instantiation
   static func freshController() -> TimeStampViewController {
-    //1.
+    
     let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
-    //2.
+    
     let identifier = NSStoryboard.SceneIdentifier("TimeStampViewController")
-    //3.
+    
     guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? TimeStampViewController else {
-      fatalError("Why cant i find TimeStampViewController? - Check Main.storyboard")
+      fatalError("Cannot find TimeStampViewController, check Main.storyboard")
     }
+    
     return viewcontroller
   }
 }
